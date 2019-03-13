6295cdb5597b0929841e7dcb29be20bac00ba0a6
Greg Bowler <greg.bowler@g105b.com>
2019-03-13 10:46:06 +0000

    Test defaults for SelectQuery

--- src/SelectQuery.php
+++ src/SelectQuery.php
@@ -15,56 +15,6 @@ abstract class SelectQuery extends SqlQuery {
 			"limit" => $this->limit(),
 			self::POST_QUERY_COMMENT => $this->postQuery(),
 		]);
-/*
-		$query = "";
-		$preQuery = $this->preQuery();
-		if(strlen($preQuery) > 0) {
-			$query .= self::PRE_QUERY_COMMENT;
-			$query .= $preQuery;
-			$query .= PHP_EOL;
-		}
-
-		$query .= "select\n\t";
-		$query .= implode(",\n\t", $this->select());
-		$query .= PHP_EOL;
-
-		$query .= "from\n\t";
-		$query .= implode(",\n\t", $this->from());
-		$query .= PHP_EOL;
-
-		$where = $this->where();
-		if(!empty($where)) {
-			$query .= "where\n\t";
-// TODO: In where(), return [new And("1=1", "2=2"), new Or("1=1", "2=5")]
-			$query .= implode("\nand\n\t", $where);
-			$query .= PHP_EOL;
-		}
-
-		$groupBy = $this->groupBy();
-		if(!empty($groupBy)) {
-			$query .= "group by\n\t";
-			$query .= implode(",\n\t", $groupBy);
-			$query .= PHP_EOL;
-		}
-
-		$having = $this->having();
-		if(!empty($having)) {
-			$query .= "having\n\t";
-// TODO: In having(), return [new And("1=1", "2=2"), new Or("1=1", "2=5")]
-			$query .= implode("\nand\n\t", $having);
-			$query .= PHP_EOL;
-		}
-
-		$postQuery = $this->postQuery();
-		if(strlen($postQuery) > 0) {
-			$query .= PHP_EOL;
-			$query .= self;
-			$query .= $postQuery;
-			$query .= PHP_EOL;
-		}
-
-		return $query;
-		*/
 	}
 
 	/** @return string[]|SqlQuery[] */
---
+++ test/unit/SelectQueryTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Gt\SqlBuilder\Test;
+
+use Gt\SqlBuilder\SelectQuery;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class SelectQueryTest extends TestCase {
+	public function testDefaults() {
+		/** @var MockObject|SelectQuery $sut */
+		$sut = self::getMockForAbstractClass(SelectQuery::class);
+		self::assertEmpty($sut->select());
+		self::assertEmpty($sut->from());
+		self::assertEmpty($sut->where());
+		self::assertEmpty($sut->groupBy());
+		self::assertEmpty($sut->having());
+		self::assertEmpty($sut->window());
+		self::assertEmpty($sut->orderBy());
+		self::assertEmpty($sut->limit());
+	}
+}
