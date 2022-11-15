32ccdba768a8817a3f6129e86f6f7ac7cef9069d
Greg Bowler <greg.bowler@g105b.com>
2022-11-15 16:06:58 +0000

    refactor: dynamic build parts

--- src/Query/SelectQuery.php
+++ src/Query/SelectQuery.php
@@ -1,7 +1,12 @@
 <?php
 namespace Gt\SqlBuilder\Query;
 
+use Gt\SqlBuilder\Condition\Condition;
+
 abstract class SelectQuery extends SqlQuery {
+	/** @var array<string, array<string|Condition>|int> */
+	private array $dynamicParts;
+
 	public function __construct(
 		protected bool $subQuery = false
 	) {}
@@ -35,78 +40,96 @@ abstract class SelectQuery extends SqlQuery {
 		return $query;
 	}
 
+	/** @param array<string, array<string|Condition>|int> $parts */
+	public function setDynamicParts(array $parts):void {
+		$this->dynamicParts = $parts;
+	}
+
 	/** @return string[]|SqlQuery[] One or more `select_expr`, typically a list of columns */
 	public function select():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[]|SqlQuery[] One or more `table_reference` */
 	public function from():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function innerJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function crossJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function straightJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function leftJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function leftOuterJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function rightJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `join_specification` */
 	public function rightOuterJoin():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return array<int|string, int|string|SqlQuery> Zero or more `where_condition` */
 	public function where():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `col_name | expr | position` */
 	public function groupBy():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return array<int|string, int|string|SqlQuery> Zero or more `having_condition` */
 	public function having():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return string[] Zero or more `col_name | expr | position` */
 	public function orderBy():array {
-		return [];
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return ?int `row_count` */
 	public function limit():?int {
-		return null;
+		return $this->dynamicReturn(__FUNCTION__);
 	}
 
 	/** @return ?int `offset` */
 	public function offset():?int {
-		return null;
+		return $this->dynamicReturn(__FUNCTION__);
+	}
+
+	protected function dynamicReturn(string $functionName):array|int|null {
+		$functionName = str_replace("_", " ", $functionName);
+		$functionName = ucwords($functionName);
+		$functionName = str_replace(" ", "", $functionName);
+		$functionName = lcfirst($functionName);
+
+		$default = ($functionName === "limit" || $functionName === "offset")
+			? null
+			: [];
+
+		return $this->dynamicParts[$functionName] ?? $default;
 	}
 }
--- src/SelectBuilder.php
+++ src/SelectBuilder.php
@@ -1,7 +1,6 @@
 <?php
 namespace Gt\SqlBuilder;
 
-use Gt\SqlBuilder\Condition\Condition;
 use Gt\SqlBuilder\Query\SelectQuery;
 
 class SelectBuilder extends AbstractQueryBuilder {
@@ -14,6 +13,7 @@ class SelectBuilder extends AbstractQueryBuilder {
 		"leftOuterJoin" => [],
 		"rightJoin" => [],
 		"rightOuterJoin" => [],
+		"straightJoin" => [],
 		"where" => [],
 		"groupBy" => [],
 		"having" => [],
@@ -25,69 +25,8 @@ class SelectBuilder extends AbstractQueryBuilder {
 	// NOTE: This doesn't need to do any lazy loading. THat's the job of the
 	// database to decide what query to build.
 	public function __toString():string {
-		return new class($this->parts) extends SelectQuery {
-			/** @param array<string, string[]|Condition[]>|array<string, ?int> $parts */
-			public function __construct(
-				private array $parts
-			) {
-				parent::__construct();
-			}
-
-			function select():array {
-				return $this->parts["select"];
-			}
-
-			function from():array {
-				return $this->parts["from"];
-			}
-
-			function innerJoin():array {
-				return $this->parts["innerJoin"];
-			}
-
-			function crossJoin():array {
-				return $this->parts["crossJoin"];
-			}
-
-			function leftJoin():array {
-				return $this->parts["leftJoin"];
-			}
-
-			function leftOuterJoin():array {
-				return $this->parts["leftOuterJoin"];
-			}
-
-			function rightJoin():array {
-				return $this->parts["rightJoin"];
-			}
-
-			function rightOuterJoin():array {
-				return $this->parts["rightOuterJoin"];
-			}
-
-			function where():array {
-				return $this->parts["where"];
-			}
-
-			function groupBy():array {
-				return $this->parts["groupBy"];
-			}
-
-			function having():array {
-				return $this->parts["having"];
-			}
-
-			function orderBy():array {
-				return $this->parts["orderBy"];
-			}
-
-			function limit():?int {
-				return $this->parts["limit"];
-			}
-
-			function offset():?int {
-				return $this->parts["offset"];
-			}
-		};
+		$class = new class() extends SelectQuery {};
+		$class->setDynamicParts($this->parts);
+		return (string)$class;
 	}
 }
