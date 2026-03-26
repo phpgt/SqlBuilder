47691d36d0d768ff1bf6307d05c1a1c4d89fe53a
Greg Bowler <greg@g105b.com>
2026-03-26 17:23:27 +0000

    test: more tests for alter table for #40

--- src/Query/Table/AlterTableQuery.php
+++ src/Query/Table/AlterTableQuery.php
@@ -2,13 +2,21 @@
 namespace Gt\SqlBuilder\Query\Table;
 
 use Gt\SqlBuilder\Query\SqlQuery;
+use Gt\SqlBuilder\SqlBuilderException;
 
 abstract class AlterTableQuery extends SqlQuery {
 	public function __toString():string {
+		$alterOptions = $this->alterOptions();
+		if(empty($alterOptions)) {
+			throw new SqlBuilderException(
+				"AlterTableQuery requires at least one alter option"
+			);
+		}
+
 		return $this->processClauseList([
 			self::PRE_QUERY_COMMENT => $this->preQuery(),
 			"alter table" => $this->alterTable(),
-			"alter options" => $this->alterOptions(),
+			"alter options" => $alterOptions,
 			self::POST_QUERY_COMMENT => $this->postQuery(),
 		]);
 	}
--- test/phpunit/Query/Table/AlterTableQueryTest.php
+++ test/phpunit/Query/Table/AlterTableQueryTest.php
@@ -1,13 +1,66 @@
 <?php
 namespace Gt\SqlBuilder\Test\Query\Table;
 
+use Gt\SqlBuilder\Query\SqlQuery;
+use Gt\SqlBuilder\SqlBuilderException;
 use Gt\SqlBuilder\Test\Helper\Query\Table\AlterTableExample;
 use Gt\SqlBuilder\Test\QueryTestCase;
 
 class AlterTableQueryTest extends QueryTestCase {
-	public function testAlterTableSimple() {
+	public function testAlterTableSimple():void {
 		$sut = new AlterTableExample();
 		$sql = self::normalise($sut);
 		self::assertSame("alter table student add column telephone int after dob, add column email varchar(256) after id", $sql);
+	}
+
+	public function testAlterTableWithoutOptionsThrowsException():void {
+		$sut = new class extends AlterTableExample {
+			public function alterOptions():array {
+				return [];
+			}
+		};
+
+		self::expectException(SqlBuilderException::class);
+		self::expectExceptionMessage(
+			"AlterTableQuery requires at least one alter option"
+		);
+		$sut->__toString();
+	}
+
+	public function testAlterTableWithPreAndPostQueryComments():void {
+		$sut = new class extends AlterTableExample {
+			public function preQuery():string {
+				return "/* before */";
+			}
+
+			public function postQuery():string {
+				return "/* after */";
+			}
+		};
+
+		self::assertSame(
+			"/* preQuery */ /* before */ alter table student add column telephone int after dob, add column email varchar(256) after id/* postQuery */ /* after */",
+			self::normalise($sut)
+		);
+	}
+
+	public function testAlterTableAllowsSqlQueryOptions():void {
+		$sut = new class extends AlterTableExample {
+			public function alterOptions():array {
+				return [
+					"add column telephone int after dob",
+					new class extends SqlQuery {
+						public function __toString():string {
+							return "drop column email";
+						}
+					},
+				];
+			}
+		};
+
+		self::assertSame(
+			"alter table student add column telephone int after dob, drop column email",
+			self::normalise($sut)
+		);
 	}
 }
